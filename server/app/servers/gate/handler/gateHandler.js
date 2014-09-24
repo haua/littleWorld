77cc41a4599d9aa4569003adb822dc0139f91bf1
Haohua Huang <hhhaua@gmail.com>
2014-09-24 11:26:00 +0800

    给服务端的几个js添加注释
    给服务端的几个js添加注释

--- server/app/servers/gate/handler/gateHandler.js
+++ server/app/servers/gate/handler/gateHandler.js
@@ -1,3 +1,5 @@
+//此js的作用是给客户点分配服务器
+
 var dispatcher = require('../../../util/dispatcher');
 
 module.exports = function(app) {
@@ -19,27 +21,27 @@ var handler = Handler.prototype;
  *
  */
 handler.queryEntry = function(msg, session, next) {
-	var uid = msg.uid;
-	if(!uid) {
+	var uid = msg.uid;//登录时客户端中{uid:'test'}，这个值是随意的，但是最好是用户的用户名
+	if(!uid){
 		next(null, {
 			code: 500,
-            message: '请先登陆再连接'
+            message: '请先登录再连接'
 		});
 		return;
 	}
-	// get all connectors
+	// 这里获取的就是connector服务器，具体服务器的配置是位于game-server\config\servers.json
 	var connectors = this.app.getServersByType('connector');
-	if(!connectors || connectors.length === 0) {
+	if(!connectors || connectors.length === 0) {//判断connectors是否合法，否则返回500（服务器错误）
 		next(null, {
 			code: 500
 		});
 		return;
 	}
 	// select connector
-	var res = dispatcher.dispatch(uid, connectors);
+	var res = dispatcher.dispatch(uid, connectors);//根据用户名uid和connectors数组，获取其中一个connector服务器，dispatcher.dispatch()方法在此页第一行引入
 	next(null, {
 		code: 200,
-		host: res.host,
-		port: res.clientPort
+		host: res.host,//分配的服务器的url
+		port: res.clientPort//分配的服务器的端口
 	});
 };
--- server/app/util/dispatcher.js
+++ server/app/util/dispatcher.js
@@ -1,6 +1,6 @@
 var crc = require('crc');
-
+//专门用于给客户端分配服务器的。
 module.exports.dispatch = function(uid, connectors) {
-	var index = Math.abs(crc.crc32(uid)) % connectors.length;
+	var index = Math.abs(crc.crc32(uid)) % connectors.length;//根据传入的uid随机生成的数再与connectors的长度取模(求余数)得到的所要的connector在数组的索引。
 	return connectors[index];
 };
